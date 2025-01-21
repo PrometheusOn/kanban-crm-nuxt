95c368bd831b77d5eefaa3bc80a868287b59e858
PrometheusOn <ramazanfajzov@mail.ru>
2025-01-21 08:46:28 +0500

    #1 настройка shadcn, tailwind

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="grid grid-cols-[1fr_6fr] min-h-full">
+    <LayoutSidebar />
+    <div>
+      <slot />
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+
+</script>
