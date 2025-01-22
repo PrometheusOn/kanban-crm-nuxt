b212055c8349dbc6a0176ab7b92c795dad73bfcc
PrometheusOn <ramazanfajzov@mail.ru>
2025-01-22 08:36:36 +0500

    #2 Подготовка к авторизации

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <NuxtLayout>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <!-- <NuxtRouteAnnouncer /> -->
+    <NuxtPage/>
   </NuxtLayout>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="p-10">
+    <h1 class="font-bold text-2ml mb-10">
+      CRM System by @ramazanfaizov
+    </h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
