7f96855010c0a77966122db9e4f4c39f2952bd0c
PrometheusOn <ramazanfajzov@mail.ru>
2025-01-22 07:36:08 +0500

    #2 меню

---
+++ components/layout/Menu.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <nuxt-link
+      class="flex items-center py-1 px-3 rounded-lg w-full hover:bg-gray-700 hover:shadow transition-all mb-2.5"
+      v-for="item in MENU_DATA"
+      :key="item.name"
+      :to="item.url"
+    >
+      <Icon :name="item.icon" class="mr-3" />
+      <span>{{ item.name }}</span>
+    </nuxt-link>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { MENU_DATA } from './menu.data';
+
+
+</script>
--- components/layout/sidebar.vue
+++ components/layout/sidebar.vue
@@ -1,9 +1,12 @@
 <template>
   <aside class="px-5 py-8 bg-sidebar h-full w-full relative">
     <NuxtLink to="/" class="mb-10 block">
-      <NuxtImg src="/logo.svg" alt="" width="100px" class="mx-auto" />
-      <UiButton />
+      <NuxtImg src="/logo.svg" alt="" width="120px" class="mx-auto" />
     </NuxtLink>
+    <button class="absolute top-2 right-3 transition-colors hover:text-primary">
+      <Icon name="line-md:logout" size="20" />
+    </button>
+    <LayoutMenu />
   </aside>
 </template>
 
