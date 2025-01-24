313203fac9d8abd53efdc18ea7ddb448638c79b8
PrometheusOn <ramazanfajzov@mail.ru>
2025-01-24 07:49:05 +0500

    #3 Добавил карточку и все остальные chadcn компоненты

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,13 @@
     <h1 class="font-bold text-2ml mb-10">
       CRM System by @ramazanfaizov
     </h1>
+    <div>
+      <UiCard class="mb-3" draggable="true">
+        <UiCardHeader>name card</UiCardHeader>
+        <UiCardContent>company</UiCardContent>
+        <UiCardFooter>date</UiCardFooter>
+      </UiCard>
+    </div>
   </div>
 </template>
 
